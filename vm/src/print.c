d6c6a5d28a9b7d27e7a11a69da1169366f544dc2
Keanu Carrot <kcarrot@e2r12p3.21-school.ru>
2019-02-19 11:51:33 +0300

    print 32

--- vm/src/print.c
+++ vm/src/print.c
@@ -6,7 +6,7 @@
 /*   By: kcarrot <kcarrot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 15:28:44 by kcarrot           #+#    #+#             */
-/*   Updated: 2019/02/18 22:25:18 by kcarrot          ###   ########.fr       */
+/*   Updated: 2019/02/19 11:51:21 by kcarrot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	ft_print_memory(unsigned char *mem, unsigned char *colors, int size)
 		ft_putchar((*mem & 0xf) + ((*mem & 0xf) > 9 ? 'A' - 10 : '0'));
 		ft_putchar(' ');
 		mem++;
-		(!((i + 1) % 64)) ? ft_putendl("") : (void)0;
+		(!((i + 1) % 32)) ? ft_putendl("") : (void)0;
 	}
 }
 
